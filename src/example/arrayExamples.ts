e518fe432615583680fd5bccf3fefe4893f0e2ce
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2022-09-02 21:49:27 +0900

    Update tsconfig.json and more

--- src/example/arrayExamples.ts
+++ src/example/arrayExamples.ts
@@ -1,8 +1,8 @@
 function arrayExamples() {
-  const xs = ['a', 'b', 'c']
-  const ys = xs.map(x => x.toUpperCase())
-  console.log(ys) // [ A, B, C ]
+  const xs = ['a', 'b', 'c'];
+  const ys = xs.map(x => x.toUpperCase());
+  console.log(ys); // [ A, B, C ]
 
-  const zs = xs.map((x, i) => x.toUpperCase() + i)
+  const zs = xs.map((x, i) => x.toUpperCase() + i);
   console.log(zs); // [ A0, B1, C2 ]
 }
