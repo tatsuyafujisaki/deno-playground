c6660bcbada9b8a5f4f5d0a25249c0bf6647a22f
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2023-01-22 15:05:23 +0900

    Add chrome.ts

---
+++ src/util/chrome.ts
@@ -0,0 +1,14 @@
+function deleteAttributesFromChromeBookmarks() {
+  for (const e of document.getElementsByTagName('h3')) {
+    e.removeAttribute('add_date');
+    e.removeAttribute('last_modified');
+    e.removeAttribute('personal_toolbar_folder');
+  }
+
+  for (const e of document.getElementsByTagName('a')) {
+    e.removeAttribute('add_date');
+    e.removeAttribute('icon');
+  }
+}
+
+deleteAttributesFromChromeBookmarks()
