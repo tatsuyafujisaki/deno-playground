a638917f2c9bb55825b24c5c437544a14d005ef0
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2023-11-28 20:18:37 +0900

    Create printJsonExamples.ts

---
+++ src/example/json/printJsonExamples.ts
@@ -0,0 +1,16 @@
+function printJsonRootType(json: string) {
+  let parsed = JSON.parse(json);
+  console.log(parsed == null ? 'null' : Array.isArray(parsed) ? 'array' : typeof parsed);
+}
+
+printJsonRootType('null'); // null
+
+printJsonRootType('true'); // true
+printJsonRootType('false'); // false
+
+printJsonRootType('-1'); // number
+printJsonRootType('3.14'); // number
+
+printJsonRootType('{ "name": "Jane", "age": 18, "hobby": null }') // object
+
+printJsonRootType('["a", "b"]'); // array
